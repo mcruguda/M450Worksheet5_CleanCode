a3ab502c986189da413b1c5e14ab2000a56b8721
Cyrill Augustiny <c.augustiny@gmail.com>
2024-09-16 15:26:29 +0200

    cleaned up books file

--- unclean_code_books.js
+++ unclean_code_books.js
@@ -1,41 +1,35 @@
-// Poorly written JavaScript file for handling book transactions
+// Not Poorly written JavaScript file for handling book transactions
 
-let bks = [
-  { i: 1, n: "Der alte Mann und das Meer", p: 10.5 },
-  { i: 2, n: "Moby Dick", p: 12.3 },
-  { i: 3, n: "1984", p: 8.5 },
+let books = [
+  { id: 1, name: "Der alte Mann und das Meer", price: 10.5 },
+  { id: 2, nname: "Moby Dick", price: 12.3 },
+  { id: 3, name: "1984", price: 8.5 },
 ];
 
-function getB(id) {
-  for (let b of bks) {
-    // Compares two values
-    if (b.i == id) {
-      return b;
+function getBook(id) {
+  for (let book of books) {
+    if (book.id == id) {
+      return book;
     }
   }
-  // Returns null
   return null;
 }
 
-function aB(name, price) {
-  // The maxId
+function addBook(name, price) {
   let maxId = 0;
-  for (let b of bks) {
-    if (b.i > maxId) maxId = b.i;
+  for (let book of books) {
+    if (book.id > maxId) maxId = book.id;
   }
-  // Pushes a new item into an array
-  bks.push({ i: maxId + 1, n: name, p: price });
+  books.push({ id: maxId + 1, name: name, price: price });
 }
 
-function lB() {
-  for (let b of bks) {
-    // Outputs something to the console
-    console.log(b.n + " - " + b.p + "€");
+function listBooks() {
+  for (let book of books) {
+    console.log(book.name + " - " + book.price + "€");
   }
 }
 
-// Example Function Calls
-let myB = getB(2);
-console.log(myB);
-aB("Fahrenheit 451", 9.2);
-lB();
+let myBook = getBook(2);
+console.log(myBook);
+addBook("Fahrenheit 451", 9.2);
+listBooks();
